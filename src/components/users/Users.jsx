15589969ab833da4343ba37cf4b33d517351e017
Oleksandr Rulenko <rulenko4@gmail.com>
2020-05-31 21:40:09 +0200

    componentDidMount. life cycle methods.

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -6,12 +6,10 @@ import userPhoto from '../assets/icons/avatar.png'
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);  
-        
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            });          
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        });  
     }
 
     render() {
