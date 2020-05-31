33c106118fbcebd938577c129ceaf1c5c184ea50
Oleksandr Rulenko <rulenko4@gmail.com>
2020-05-31 20:05:47 +0200

    Class component. (extends React.Component)

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import s from './Menu.module.css'
 import { NavLink } from 'react-router-dom'
+import iconHome from '../assets/icons/home.png'
+import iconMessage from '../assets/icons/message.png'
+import iconFriends from '../assets/icons/friends.png'
 
 const Menu = () => {
     return (   
       <nav className={s.menu}>
         <NavLink to='/profile' activeClassName={s.activeLink}>
           <div className={`${s.menuItems} ${s.hoverBGColor}`}>
-            <div className={s.menuItemIcon}><img src="https://sun9-8.userapi.com/c629327/v629327473/db66/r051joYFRX0.jpg?ava=1" alt=""/></div>
+            <div className={s.menuItemIcon}><img src={ iconHome } alt=""/></div>
             <div className={s.menuItemLink}>Profile</div>
             <div className={s.menuItemCounter}>hh</div>
           </div>
         </NavLink>
         <NavLink to='/dialogs' activeClassName={s.activeLink}>
           <div className={`${s.menuItems} ${s.hoverBGColor}`}>
-            <div className={s.menuItemIcon}><img src="https://sun9-8.userapi.com/c629327/v629327473/db66/r051joYFRX0.jpg?ava=1" alt=""/></div>
+            <div className={s.menuItemIcon}><img src={ iconMessage } alt=""/></div>
             <div className={s.menuItemLink}>Dialogs</div>
+            <div className={s.menuItemCounter}>hh</div>
+          </div>
+        </NavLink>
+        <NavLink to='/users' activeClassName={s.activeLink}>
+          <div className={`${s.menuItems} ${s.hoverBGColor}`}>
+            <div className={s.menuItemIcon}><img src={ iconFriends } alt=""/></div>
+            <div className={s.menuItemLink}>Users</div>
             <div className={s.menuItemCounter}>hh</div>
           </div>
         </NavLink>
--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -4,37 +4,39 @@ import * as axios from 'axios';
 import userPhoto from '../assets/icons/avatar.png'
 
 
-let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
+class Users extends React.Component {
 
+    constructor(props) {
+        super(props);  
+        
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-            });
-        }
+                this.props.setUsers(response.data.items)
+            });          
     }
 
-    return <div>
-        <button onClick={getUsers}>Get Users</button>
-        {
-            props.users.map( u => <div key={u.id} className={s.userItem}> 
-                <div className={s.userAvatar}>
-                    <img src={ u.photos.small != null ? u.photos.small : userPhoto} alt='' />
-                </div>
-                <div>
-                    <div className={s.userName}>{ u.name }</div>
-                    <div className={s.userStatus}>{ u.status }</div>
-                    {/* <div className={s.userLocation}>{ u.location.city } { u.location.country }</div> */}
-                </div>
-                <div className={s.userFollow}>
-                    { u.followed
-                     ? <button onClick={ () => { props.unfollow(u.id) } } >Unfollow</button>
-                     : <button onClick={ () => { props.follow(u.id) } } >Follow</button>}
-                </div>
-            </div>)
-        }
-    </div>
+    render() {
+        return <div>
+            {
+                this.props.users.map( u => <div key={u.id} className={s.userItem}> 
+                    <div className={s.userAvatar}>
+                        <img src={ u.photos.small != null ? u.photos.small : userPhoto} alt='' />
+                    </div>
+                    <div>
+                        <div className={s.userName}>{ u.name }</div>
+                        <div className={s.userStatus}>{ u.status }</div>
+                        {/* <div className={s.userLocation}>{ u.location.city } { u.location.country }</div> */}
+                    </div>
+                    <div className={s.userFollow}>
+                        { u.followed
+                        ? <button onClick={ () => { this.props.unfollow(u.id) } } >Unfollow</button>
+                        : <button onClick={ () => { this.props.follow(u.id) } } >Follow</button>}
+                    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 
 export default Users
 
