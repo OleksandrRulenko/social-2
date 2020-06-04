99586254fbfef667d430c69a009228312ca9636a
Oleksandr Rulenko <rulenko4@gmail.com>
2020-06-04 21:38:37 +0200

    follow - unfollow api

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Users.module.css';
 import userPhoto from '../assets/icons/avatar.png'
 import { NavLink } from 'react-router-dom';
-
+import * as axios from 'axios';
 
 let Users = (props) => {
 
@@ -41,8 +41,32 @@ let Users = (props) => {
 
                     <div className={s.userFollow}>
                         { u.followed
-                        ? <button onClick={ () => { props.unfollow(u.id) } } >Unfollow</button>
-                        : <button onClick={ () => { props.follow(u.id) } } >Follow</button>}
+                        ? <button onClick={ () => { 
+                         
+                            axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                withCredentials: true,
+                                headers: { 'API-KEY': 'ce8a87aa-30e2-4320-86d2-b1029b457e9b' },
+                            })
+                            .then(response => {
+                                if (response.data.resultCode == 0)  {
+                                    props.unfollow(u.id); 
+                                }
+                            }); 
+
+                        } } >Unfollow</button>
+                        : <button onClick={ () => { 
+                        
+                            axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                withCredentials: true,
+                                headers: { 'API-KEY': 'ce8a87aa-30e2-4320-86d2-b1029b457e9b' },
+                            })
+                            .then(response => {
+                                if (response.data.resultCode == 0)  {
+                                    props.follow(u.id); 
+                                }
+                            });  
+                            
+                        } } >Follow</button>}
                     </div>
                 </div>
             )
